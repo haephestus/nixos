831183842c01153d0662b8462761d72754addc35
27Mikael <vitalybodgan@gmail.com>
2026-02-05 05:18:34 +0200

    new plugin files: conform, iron, outline, todo

--- dotfiles/nvim/lua/plugins/conform.lua
+++ dotfiles/nvim/lua/plugins/conform.lua
@@ -3,7 +3,13 @@ return {
   optional = true,
   opts = {
     formatters_by_ft = {
-      ["python"] = { "isort", "black" },
+      python = { "isort", "black" },
+
+      html = { "prettier" },
+      css = { "prettier" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      json = { "prettier" },
     },
   },
 }
